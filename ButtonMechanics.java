4024475bb3e0c22ccf6381564724c610e5a143eb
Sebastian <2224@ikg.local>
2022-12-14 10:19:37 +0100

    kleien text änderung mechanics

--- ButtonMechanics.java
+++ ButtonMechanics.java
@@ -8,7 +8,8 @@ public class ButtonMechanics extends Button {
     @Override
     public void onClick() {
         
-        System.out.println("Nö");        
+        System.out.println("Es geht darum, mit der Figur so schnell und oft, wie möglich, die andere Seite zu kommen und das Haus zu erreichen. Wenn man das Hausberührt, erhält du einen Spielpunkt. Welche Figur du wählst, ist eure Entscheidung. Der Weihnachtsmann bewegt sich mit w,a,s,d, während sich Rudolf, das Retier, mit den Pfeilstastern die Wege bahnt. Aber Vorsicht! Die Autos stellen eine Gefahr da, während die Schlitten euch helfen, das Gebirge zu überwinden.");
+        
     }
     
 }
